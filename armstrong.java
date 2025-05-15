b3828c870ab5ce1e7ba91dce3c7104072e0336ff
sharkiiee <sarthak231203@gmail.com>
2025-05-15 18:37:55 +0530

    this is person 2 palindrome

--- armstrong.java
+++ armstrong.java
@@ -1,5 +1,5 @@
 public class armstrong {
     public static void main(String[] args) {
-        System.out.println("I am in the person ");
+        System.out.println("I am in the person2 person ");
     }
 }
